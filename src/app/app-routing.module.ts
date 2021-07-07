ed47686cb0c3f07225d27c5c8a16d4a20985cc60
Dieter Jackson <jacksondieter@gmail.com>
2021-07-07 20:32:25 +0200

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutFormComponent } from './components/checkout-form/checkout-form.component';
+import { OrderConfirmationComponent } from './components/order-confirmation/order-confirmation.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: ShoppingCartComponent },
+  { path: 'checkout', component: CheckoutFormComponent },
+  { path: 'confirmation', component: OrderConfirmationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
