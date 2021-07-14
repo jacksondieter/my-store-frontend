1425b6eca009bb48b645716cac07c55997050693
Dieter Jackson <jacksondieter@gmail.com>
2021-07-14 11:23:59 +0200

    feat: Add shopping cart

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { CartListItem } from 'src/app/models/cartItem';
+import { CartService } from 'src/app/service/cart.service';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css','../../app.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
-
-  constructor() { }
+  cart: CartListItem[] = []
+  quantity: number = 0
+  total:number = 0
+  constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
+    this.cartService.quantityService$.subscribe((itemsTotal) => {
+    this.quantity = itemsTotal.quantity
+    this.total = itemsTotal.total
+    this.cart = itemsTotal.cart
+    })
   }
 
+  upQuantity(id:number):void{
+    this.cartService.upQuantity(id)
+  }
+  
+  downQuantity(id:number):void{
+    this.cartService.downQuantity(id) 
+  }
+
+  deleteFromCart(id:number){
+    this.cartService.deleteFromCart(id)
+  }
+
+  onCheckoutHandler(){
+    if (this.cart.length > 0) {
+      this.router.navigateByUrl('/checkout')
+    }
+  }
 }
