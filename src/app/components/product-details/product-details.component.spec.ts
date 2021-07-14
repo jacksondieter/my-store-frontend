50e33352901859edf4fd21d17db20e85c156cbc8
Dieter Jackson <jacksondieter@gmail.com>
2021-07-14 11:22:45 +0200

    feat: Add products details

--- src/app/components/product-details/product-details.component.spec.ts
+++ src/app/components/product-details/product-details.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ProductDetailsComponent } from './product-details.component';
+import { Observable } from 'rxjs';
 
 describe('ProductDetailsComponent', () => {
   let component: ProductDetailsComponent;
@@ -8,9 +11,16 @@ describe('ProductDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductDetailsComponent ]
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      declarations: [ProductDetailsComponent],
+      providers:[{
+        provide: ActivatedRoute,
+        useValue: {
+          params: new Observable(suscriber => suscriber.next({ id: '1' })) 
+        }
+      }]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/service/cart.service';
+import { HttpService } from 'src/app/service/http.service';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css','../../app.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  id: number = 1
+  product: Product = new Product()
+  quantity:number = 1
+  constructor(private route: ActivatedRoute, private http: HttpService, private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap(params => {
+          this.id = +params.id
+          return this.http.getProduct(this.id)}))
+      .subscribe(data => {this.product = data})
   }
 
+  upQuantity():void{
+    this.quantity +=1
+  }
+
+  downQuantity():void{
+    if(this.quantity > 1){
+      this.quantity -=1
+    } 
+  }
+
+  addCart(){
+    const cartItem = {...this.product, quantity:this.quantity}
+    this.cartService.addToCart(cartItem)
+  }
+
+  onCloseHandler(){
+    this.router.navigateByUrl('/')
+  }
 }
